95648b91a836ecb1f365f9b5343864235edf4a4a
David Waelder <werwerf@gmail.com>
2017-01-16 15:46:21 +0100

    Added comments to make it understandable

--- Update-parent.py
+++ Update-parent.py
@@ -1,8 +1,13 @@
-from com.atlassian.jira.component import ComponentAccessor
-from com.atlassian.jira.issue import CustomFieldManager
+# Update-parent.py
+# (c) David Waelder 2016 - 2017
+#
+# This script takes the value of the Original Estimate field of the subtask
+# and copies it to a custom field in its parent issue.
+
+from com.atlassian.jira.component import ComponentAccessor      # ComponentAccessor enables access to the Custom Fields objects, that is global
+from com.atlassian.jira.issue import CustomFieldManager         # 
 from com.atlassian.jira.issue.fields import CustomField
 
-customFieldManager = ComponentAccessor.getCustomFieldManager()
 customField = customFieldManager.getCustomFieldObject(12731)
 
 estimation = round(issue.getOriginalEstimate()/3600,2)
