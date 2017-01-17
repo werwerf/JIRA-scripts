db2756173cdfc5bb3a2d743e5b4b6ec75d969fba
David Waelder <werwerf@gmail.com>
2017-01-17 18:31:56 +0100

    Bug fixing
    Fixed issue with missing call to ComponentAccessor to get the Custom fields manager object.

--- Update-parent.py
+++ Update-parent.py
@@ -4,11 +4,15 @@
 # This script takes the value of the Original Estimate field of the subtask
 # and copies it to a custom field in its parent issue.
 
-# CustomFieldManager enables access to the Custom Fields objects, that is global object.
+# Import global objects needed to access the data.
+from com.atlassian.jira.component import ComponentAccessor
 from com.atlassian.jira.issue import CustomFieldManager
 
+# my_customFieldManager get the Custom Field Manager object to access custom fields
+my_customFieldManager = ComponentAccessor.getCustomFieldManager()
+
 # my_customField gets the desired custom field (customfield_12731) object.
-my_customField = customFieldManager.getCustomFieldObject(12731)
+my_customField = my_customFieldManager.getCustomFieldObject(12731)
 
 # We get the Original Estimate value (in seconds) and convert it into hours (two decimals).
 the_estimation = round(issue.getOriginalEstimate()/3600,2)
